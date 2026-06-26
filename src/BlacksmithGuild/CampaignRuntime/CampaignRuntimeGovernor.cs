b7ad0e35bb55379b4d18bdd82da8aeb2db9dc40c
EndeavorEverlasting <71802818+EndeavorEverlasting@users.noreply.github.com>
2026-06-26 19:56:22 -0400

    fix(governor): import map transition guard namespace

--- src/BlacksmithGuild/CampaignRuntime/CampaignRuntimeGovernor.cs
+++ src/BlacksmithGuild/CampaignRuntime/CampaignRuntimeGovernor.cs
@@ -1,6 +1,7 @@
 using System;
 using BlacksmithGuild.DevTools;
 using BlacksmithGuild.DevTools.Automation;
+using BlacksmithGuild.DevTools.Reporting;
 using BlacksmithGuild.Food;
 
 namespace BlacksmithGuild.CampaignRuntime
